77f476ae23a2bb42a2e4c24a1a544f47f26f8241
Dan Samama <dan.samama@gmail.com>
2016-06-28 17:51:34 +0300

    First commit Adding new pictures Geeting my functions to be more generic

--- JS/BuzzQuizz.js
+++ JS/BuzzQuizz.js
@@ -1,77 +1,66 @@
 /**
  * Created by itc_user on 6/15/2016.
  */
-var checkMyScore = function () {
-    var sum = 0;
-    var allAnswers1 = document.getElementsByName("question1");
-    for (var i = 0; i < allAnswers1.length; i++) {
-        var currentAnswer1 = allAnswers1[i];
-        if (currentAnswer1.checked) {
-            sum += parseInt(currentAnswer1.value);
-        }
-        else{
-            sum += NaN;
-        }
+var sum = 0;
 
+// var answerOneResult = function () {
+//     var allAnswers1 = document.getElementsByName("question1");
+//     for (var i = 0; i < allAnswers1.length; i++) {
+//         var currentAnswer1 = allAnswers1[i];
+//         if (currentAnswer1.checked) {
+//             sum += parseInt(currentAnswer1.value);
+//         }
+//         // else {
+//         //     sum += NaN;
+//         // }
+//
+//     }
+// };
+
+// var arrayQ1Q2 = ["question1","question2"];
+//
+// var ans1ans2 = function(arrayQ1Q2){
+    
+    
+// }
+
+var handleChecked = function (num) {
+    var allAnswers = document.getElementsByName("question"+num);
+    for (var j = 0; j < allAnswers.length; j++) {
+        var currentAnswer = allAnswers[j];
+        if (currentAnswer.checked) {
+            sum += parseInt(currentAnswer.value);
+        }
     }
+};
 
-    var allAnswers2 = document.getElementsByName("question2");
-    for (var j = 0; j < allAnswers2.length; j++) {
-        var currentAnswer2 = allAnswers2[j];
-        if (currentAnswer2.checked) {
-            sum += parseInt(currentAnswer2.value);
-        }
-    }
-
-
+var answerThreeResult = function () {
     var allAnswers3 = document.getElementsByClassName("outfit selected");
     var outfitValue = allAnswers3[0].getAttribute("data-outfit-val");
     sum += parseInt(outfitValue);
+};
 
-
+var answerFourResult = function () {
     var allAnswers4 = document.getElementById("quantity");
     var houmousValue = calculateHumusValue(parseInt(allAnswers4.value));
     sum += parseInt(houmousValue);
+};
 
-
-
+var answerFiveResult = function () {
     var allAnswers5 = document.getElementById("ingredient");
-    if (ingredient.value.toLowerCase() === "eggplant"){
+    if (allAnswers5.value.toLowerCase() === "eggplant") {
         sum += 5;
     }
-    else if(ingredient.value.toLowerCase() === "egg"){
+    else if (allAnswers5.value.toLowerCase() === "egg") {
         sum += 3;
     }
-    else if(ingredient.value.toLowerCase() === "pita"){
+    else if (allAnswers5.value.toLowerCase() === "pita") {
         sum += 1;
     }
-    else{
+    else {
         sum += 0;
     }
-
-    var message = "";
-    if (isNaN(sum)){
-        message = "Please fill all the fields";
-    }else {
-        if (sum >=3 && sum < 9) {
-            message = "Are you even trying???";
-        }
-        else if(sum>=9 && sum<15){
-            message = "You have a lot of work to do";
-        }
-        else if(sum>=15 && sum<18){
-            message ="You are getting close";
-        }
-        else if(sum>=18){
-            message = "Yes you are a true Israeli, now what??? Want a gift?";
-        }
-    }
-
-    var userMessage = document.getElementById("userMessage");
-    userMessage.innerHTML = message;
-}
-
-
+};
 
 var toggleSelected = function(clickedPic){
     // var allAnswers3 = document.getElementsByClassName("outfit");
@@ -83,7 +72,7 @@ var toggleSelected = function(clickedPic){
     clickedPic.className = "outfit selected";
 
 
-}
+};
 
 var calculateHumusValue = function(houmousValue){
     var result = 0;
@@ -113,5 +102,41 @@ var calculateHumusValue = function(houmousValue){
     }
     return result;
 
-}
+};
 
+var checkPointsAnswers = function(){
+    // answerOneResult();
+    for (var i=1;i<3;i++) {
+        handleChecked(i) //Checking answer of question 1 and 2
+    }
+    // answerTwoResult('1');
+    // answerTwoResult('2');
+    answerThreeResult();
+    answerFourResult();
+    answerFiveResult();
+};
+
+var checkMyScore = function () {
+    checkPointsAnswers();
+    var message = "";
+    if (isNaN(sum)){
+        message = "Please fill all the fields";
+    }else {
+        if (sum >=3 && sum < 9) {
+            message = "Are you even trying???";
+        }
+        else if(sum>=9 && sum<15){
+            message = "You have a lot of work to do";
+        }
+        else if(sum>=15 && sum<18){
+            message ="You are getting close";
+        }
+        else if(sum>=18){
+            message = "Yes you are a true Israeli, now what??? Want a gift?";
+        }
+    }
+
+    var userMessage = document.getElementById("userMessage");
+    userMessage.innerHTML = message;
+
+};
